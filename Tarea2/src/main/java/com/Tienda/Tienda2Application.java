23c6899d7e35c3edf9d27245faa1fdc48d64eb9d
titorojas1106 <titorojas_11@hotmail.com>
2024-02-08 17:32:39 -0600

    Tarea1

---
+++ Tarea2/src/main/java/com/Tienda/Tienda2Application.java
@@ -0,0 +1,13 @@
+package com.Tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tienda2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tienda2Application.class, args);
+	}
+
+}
---
+++ Tarea2/src/test/java/com/Tienda/Tienda2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tienda2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
